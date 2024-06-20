a86c90ceb8df04a159579d21905b715dd21e7230
Ashutosh Ahmad Alexandar <thegameashutosh@gmail.com>
2024-06-20 01:21:22 +0000

    macros continued

--- models/customers_daily_summary.sql
+++ models/customers_daily_summary.sql
@@ -1,5 +1,5 @@
 select
-    {{ dbt_utils.surrogate_key(['customer_id','order_date'])}} as id, {# use as identifier #}
+    {{ dbt_utils.generate_surrogate_key(['customer_id','order_date'])}} as uid,
     customer_id,
     order_date,
     count(*),
---
+++ models/union_tables_by_orders_prefix.sql
@@ -0,0 +1,7 @@
+{{
+    union_tables_by_prefix(
+        database = 'US_DE_DB',
+        schema = 'dbt_aalexander',
+        prefix = 'stg_'
+    )
+}}
